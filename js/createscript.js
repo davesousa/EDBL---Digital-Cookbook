768c5fdf9ab330e2f87db5e435c2a9f3f3547153
Dave Sousa <davejsousa@gmail.com>
2016-02-17 17:59:40 -0500

    Create page add field
    -Add field now works
    -added container so things move dynamically
    
    Known issues:
    -EDBL logo becomes unclickable when extra fields are added?

--- js/createscript.js
+++ js/createscript.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
   $("#instructionButton").click(function() {
     $("#instructions").append('<input class="inputBox" type="text" placeholder="Add instructions here.">');
   });
+  //add field to ingredients
+  $("#ingredientsButton").click(function() {
+    $("#ingredients").append('<input class="inputBox" type="text" placeholder="Add ingredients here.">');
+  });
 });
--- js/min/createscript-min.js
+++ js/min/createscript-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#instructionButton").click(function(){$("#instructions").append('<input class="inputBox" type="text" placeholder="Add instructions here.">')})});
+$(document).ready(function(){$("#instructionButton").click(function(){$("#instructions").append('<input class="inputBox" type="text" placeholder="Add instructions here.">')}),$("#ingredientsButton").click(function(){$("#ingredients").append('<input class="inputBox" type="text" placeholder="Add ingredients here.">')})});
